303f73e0ce9e8270778c145f83e14d71fbb6e829
Umakant1901 <ukp.linux@gmail.com>
2024-07-09 21:49:08 +0530

    IQA 01

--- src/IQ/FileHandingBasedQuestion.java
+++ src/IQ/FileHandingBasedQuestion.java
@@ -1,4 +1,58 @@
 package IQ;
 
+import java.io.*;
+
 public class FileHandingBasedQuestion {
+
+
+    public void getDataFromPropertyFile() {
+
+    }
+
+    public String getDataFromTextFile(String filePath) throws IOException {
+
+        StringBuilder content = new StringBuilder();
+
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            content.append(line).append("\n");
+        }
+
+        return content.toString();
+    }
+
+
+
+
+
+    public void getDataFromExcelFile() {
+
+    }
+
+
+    public void setDataFromTextFile(String filePath,String textmessage) throws IOException {
+
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter(filePath))) {
+            writer.write(textmessage);
+        }
+
+    }
+
+    public void appendDataToTextFile(String filePath,String appendMessage) throws IOException {
+        try(BufferedWriter writer=new BufferedWriter(new FileWriter(filePath,true))){
+            writer.append(appendMessage);
+        }
+    }
+
+
+
+    public void setDataFromExcelFile(){
+
+    }
+
+
+
+
+
 }
--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,26 @@
+import IQ.FileHandingBasedQuestion;
 import IQ.OccurencyCount;
+
+import java.io.IOException;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // to check repeatation of words or character
         String str1="adsfrdwcexdsewdf";
         String str2="Hello world , welcome to real world and to learn codes";
-        OccurencyCount oc=new OccurencyCount();
-        oc.getAlphabetOccurenceInString(str1);
-        System.out.println("********");
-        oc.getWordsOccurenceInString(str2);
-        
+
+//        System.out.println(System.getProperty("user.dir"));
+
+        String filepath=System.getProperty("user.dir")+"/src/resources/NormalTextFile.txt";
+
+        FileHandingBasedQuestion fileIQ=new FileHandingBasedQuestion();
+
+        fileIQ.setDataFromTextFile(filepath,"Hello world...! \n");
+        fileIQ.appendDataToTextFile(filepath,"learning Java \n");
+        fileIQ.appendDataToTextFile(filepath,"then I will go for Python...! \n");
+        System.out.println(fileIQ.getDataFromTextFile(filepath));
+
     }
 }
